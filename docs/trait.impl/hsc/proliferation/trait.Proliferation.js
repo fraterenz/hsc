8c1e414b6f7226b090081a25217f2ec2bc01ecea
Francesco Terenzi <fra.terenz1993@gmail.com>
2024-08-21 14:54:28 +0100

    docs

--- docs/trait.impl/hsc/proliferation/trait.Proliferation.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"hsc":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/trait.impl/std/os/fd/raw/trait.AsRawFd.js
+++ docs/trait.impl/std/os/fd/raw/trait.AsRawFd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"console":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"console/struct.Term.html\" title=\"struct console::Term\">Term</a>"]]
+"console":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"console/struct.Term.html\" title=\"struct console::Term\">Term</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
